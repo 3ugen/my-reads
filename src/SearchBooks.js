963a6dc6e02eb2a74ba2f1236ece93a519c9e279
Eugene B <enbotnaru@gmail.com>
2018-09-06 12:14:19 -0700

    add state updateQuery and input filed to SearchBooks.js

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBook extends Component {
   state = {
-    query: ''
+    query: ""
   }
 
   updateQuery = (query) => {
@@ -17,21 +17,12 @@ class SearchBook extends Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                 <input
                   type="text"
                   placeholder="Search by title or author"
                   value={this.state.query}
-                  onChange={  e => this.updateQuery(e.target.value)}
-                />
-
+                  onChange={ e => this.updateQuery(e.target.value)}
+                 />
               </div>
             </div>
             <div className="search-books-results">
